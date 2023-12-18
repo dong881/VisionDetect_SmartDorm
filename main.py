c9e0232603013a857c8513de9a85160dffa7a5cd
dong881 <joe888joejoe@gmail.com>
2023-12-18 20:18:42 +0800

    透過預先訓練模型判斷

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 import RPi.GPIO as GPIO
-import mediapipe
 import cv2
 import threading
 import time
-from mediapipe.python.solutions import pose as mp_pose
+import numpy as np
+import mediapipe as mp
+from tensorflow.keras.models import load_model
+
+# 載入訓練好的模型
+model = load_model('dong_model.h5')  # 記得換成你的模型檔名
 
 # 初始化 GPIO
 GPIO_PIN = 17  # 更改為你實際使用的 GPIO pin
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(GPIO_PIN, GPIO.OUT)
 
-# 初始化 MediaPipe 人體檢測模型
-mp_pose_module = mediapipe.solutions.pose
-mp_pose = mp_pose_module.Pose()
-
 # 初始化 OpenCV 視訊捕捉
 cap = cv2.VideoCapture(0)  # 可根據需要更改視訊源
 
 # 初始化變數
 max_continuous_time = 5  # 最大持續時間，單位為秒
-person_near_camera = False
 last_detection_time = 0  # 上一次偵測到人的時間
 
 # 控制 GPIO 狀態
@@ -43,53 +42,71 @@ def read_frame():
 read_thread = threading.Thread(target=read_frame)
 read_thread.start()
 
-def is_person_near_camera(results):
-    """
-    判斷人是否靠近鏡頭，鼻子位於畫面正下方中間，靠近則返回 True。
+def preprocess_and_reshape(frame):
 
-    Parameters:
-        results: MediaPipe 人體檢測的結果
+    mp_pose = mp.solutions.pose
+    mp_face = mp.solutions.face_detection
 
-    Returns:
-        near_camera: True 表示人靠近鏡頭，False 表示人遠離鏡頭
-    """
-    # 如果偵測不到人，直接返回 False
-    if not results.pose_landmarks:
-        return False
+    # 初始化mediapipe
+    pose = mp_pose.Pose()
+    face = mp_face.FaceDetection(min_detection_confidence=0.2)
 
-    # 獲取骨架關鍵點
-    landmarks = results.pose_landmarks.landmark
+    # 轉換為RGB
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    # 取得特定部位的座標
-    nose_coords = (landmarks[mp_pose_module.PoseLandmark.NOSE].x,
-                   landmarks[mp_pose_module.PoseLandmark.NOSE].y)
+    # 進行pose detection
+    results_pose = pose.process(rgb_frame)
 
-    # 判斷鼻子是否位於畫面正下方中間位置
-    near_camera = 0.3 < nose_coords[0] < 0.7 and nose_coords[1] > 0.3
+    # 進行face detection
+    results_face = face.process(rgb_frame)
 
-    return near_camera
+    # 提取關鍵點
+    pose_landmarks = results_pose.pose_landmarks.landmark if results_pose.pose_landmarks else None
+    face_landmarks = results_face.detections if results_face.detections else None
+
+    # 將資料正規化
+    if pose_landmarks is not None and len(pose_landmarks) > 0 and face_landmarks is not None and len(face_landmarks) > 0:
+        # 確保 pose_landmarks 和 face_landmarks 的維度一致
+        pose_landmarks = [[landmark.x, landmark.y, landmark.z] for landmark in pose_landmarks]
+        face_landmarks = [[detection.location_data.relative_bounding_box.xmin,
+                            detection.location_data.relative_bounding_box.ymin,
+                            detection.location_data.relative_bounding_box.width,
+                            detection.location_data.relative_bounding_box.height] for detection in face_landmarks]
+
+        # 調整維度一致
+        min_length = min(len(pose_landmarks), len(face_landmarks))
+        pose_landmarks = np.array(pose_landmarks)[:min_length]
+        face_landmarks = np.array(face_landmarks)[:min_length]
+
+        # 進行連接
+        concatenated_data = np.concatenate([pose_landmarks, face_landmarks], axis=1)
+
+        # 注意這裡，將資料的維度降為 (7,)，假設 concatenated_data 的形狀是 (1, 7)
+        concatenated_data = np.squeeze(concatenated_data)
+        return concatenated_data
+    else:
+        # 如果提取不到資料，返回全為零的陣列（或其他預設值）
+        return np.zeros((7,))
 
 def process_frame():
-    global frame, person_near_camera, last_detection_time, GPIO_STATE, GPIO_TARGET_STATE
+    global frame, preprocess_and_reshape, last_detection_time, GPIO_STATE, GPIO_TARGET_STATE
     while True:
         if 'frame' in globals():
-            # 進行人體檢測
-            results = mp_pose.process(frame)
 
             with lock:
                 # 判斷人是否靠近鏡頭
-                if is_person_near_camera(results):
+                input_data = preprocess_and_reshape(frame)
+                prediction = model.predict(np.expand_dims(input_data, axis=0))[0]
+                if prediction > 0.7:
                     # 更新偵測到人的時間
                     last_detection_time = time.time()
-                    person_near_camera = True
                     GPIO_TARGET_STATE = True
                 else:
                     # 如果持續時間超過設定的最大持續時間，則設定為 False
                     if time.time() - last_detection_time > max_continuous_time:
-                        person_near_camera = False
                         GPIO_TARGET_STATE = False
 
-                print(person_near_camera)
+                print(prediction)
 
                 # 控制 GPIO 狀態漸進過渡
                 if GPIO_STATE != GPIO_TARGET_STATE:
